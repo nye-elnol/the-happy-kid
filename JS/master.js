83fa1145bacd70c2e0eecf187bd860d44d61b960
limelnoll <abdmd819@gmail.com>
2021-06-11 10:41:43 +0300

    Working on Back to Top Button

--- JS/master.js
+++ JS/master.js
@@ -2,8 +2,38 @@ const navTog = document.querySelector('.nav-tog');
 const navLx = document.querySelectorAll('.nav__lk');
 
 navTog.addEventListener('click', () => {
+
   document.body.classList.toggle('nav-open');
+  
 });
+
+// Let's set up the back to top button
+mybutton = document.getElementById("bckTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+  if (document.body.scrollTop > 990 || document.documentElement.scrollTop > 990) {
+
+    mybutton.style.display = "block";
+
+  } else {
+
+    mybutton.style.display = "none";
+
+  }
+
+}
+
+
+function topFunction() {
+
+  document.body.scrollTop = 0; 
+
+  document.documentElement.scrollTop = 0; 
+
+}
 
 navLx.forEach(link => {
 
@@ -14,4 +44,5 @@ navLx.forEach(link => {
   });  
 
 });
+
 
